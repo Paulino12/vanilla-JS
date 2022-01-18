671811c1e5d23db4e22ca1d5256ebe566e17064b
Lino <Pchuitcheu@yahoo.fr>
2022-01-18 18:35:49 +0000

    more work

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -14,6 +14,7 @@ const reset = () => {
 
 // generate rgb from 3 hex values
 const hexToRgb3 = (inputValue) => {
+
     let index1 = colorDigits.findIndex(elt => elt === inputValue[0])
     let index2 = colorDigits.findIndex(elt => elt === inputValue[1])
     let index3 = colorDigits.findIndex(elt => elt === inputValue[2])
@@ -28,19 +29,51 @@ const hexToRgb3 = (inputValue) => {
     }
 }
 
+// generate rgb from 6 values
+const hexToRgb6 = (inputValue) => {
+    let index1 = colorDigits.findIndex(elt => elt === inputValue[0])
+    let index2 = colorDigits.findIndex(elt => elt === inputValue[2])
+    let index3 = colorDigits.findIndex(elt => elt === inputValue[4])
+
+    let indexA = colorDigits.findIndex(elt => elt === inputValue[1])
+    let indexB = colorDigits.findIndex(elt => elt === inputValue[3])
+    let indexC = colorDigits.findIndex(elt => elt === inputValue[5])
+
+    let a = (index1 * 16) + indexA
+    let b = (index2 * 16) + indexB
+    let c = (index3 * 16) + indexC
+
+    if(a === -17 || b === -17 || c === -17){
+        return ``
+    }else{
+        return `(${a}, ${b}, ${c})`
+    }
+
+}
+
 hexInput.addEventListener('input', (e) => {
     let thisInput = e.target.value
-    if(thisInput.length !== 3){
-        areaColor.style.backgroundColor = `#fff`
-        rgbValues.innerHTML = ""
-        notification.innerHTML = ""
-        return
-    }else{
+   if(thisInput.length === 3){
         if(hexToRgb3(thisInput) === ''){
             notification.innerHTML = "Does not exists"
+            notification.classList.add('alert', 'alert-danger', 'mb-0', 'p-2', 'rounded')
         }else{
             areaColor.style.backgroundColor = `#${thisInput}`
             rgbValues.innerHTML = hexToRgb3(thisInput)
         }
+    }else if(thisInput.length === 6){
+        if(hexToRgb6(thisInput) === ''){
+            notification.innerHTML = "Does not exists"
+            notification.classList.add('alert', 'alert-danger', 'mb-0', 'p-2', 'rounded')
+        }else{
+            areaColor.style.backgroundColor = `#${thisInput}`
+            rgbValues.innerHTML = hexToRgb6(thisInput)
+        }
+    }else{
+        areaColor.style.backgroundColor = `#fff`
+        rgbValues.innerHTML = ""
+        notification.innerHTML = ""
+        notification.classList.remove('alert', 'alert-danger', 'mb-0', 'p-2', 'rounded')
+        return
     }
 })
