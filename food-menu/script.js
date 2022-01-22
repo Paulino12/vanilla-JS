3417d5f455acc16df84747e4ed7a820abefe7040
Lino <Pchuitcheu@yahoo.fr>
2022-01-22 01:59:14 +0000

    food images

--- food-menu/script.js
+++ food-menu/script.js
@@ -4,7 +4,7 @@ let foodObjects = [
         id: 1,
         category: "Breakfast",
         name: "Egg On Toast",
-        price: "£12.50",
+        price: "£6.50",
         image: "./assets/images/eggOnToast.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae tempor dui, eu viverra libero. Integer dictum non orci sit amet eleifend"
     },
@@ -12,7 +12,7 @@ let foodObjects = [
         id: 2,
         category: "Lunch",
         name: "Poke Bowl",
-        price: "£12.50",
+        price: "£19.50",
         image: "./assets/images/pokeBowl.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae tempor dui, eu viverra libero."
     },
@@ -20,7 +20,7 @@ let foodObjects = [
         id: 3,
         category: "Dinner",
         name: "Citrus Salmon",
-        price: "£12.50",
+        price: "£17.50",
         image: "./assets/images/citrusSalmon.jpg",
         description: "Aenean mattis nisl erat, at porta ipsum dictum non. Morbi pellentesque malesuada ornare. Nunc rhoncus lectus et sollicitudin tempus."
     },
@@ -28,33 +28,33 @@ let foodObjects = [
         id: 4,
         category: "Breakfast",
         name: "French Toast",
-        price: "£12.50",
+        price: "£8.50",
         image: "./assets/images/frenchToast.jpg",
         description: "Aenean mattis nisl erat, at porta ipsum dictum non. Morbi pellentesque malesuada ornare. Nunc rhoncus lectus et sollicitudin tempus."
     },
     {
         id: 5,
-        category: "Breakfast",
-        name: "French Toast",
-        price: "£12.50",
-        image: "./assets/images/frenchToast.jpg",
+        category: "Lunch",
+        name: "Parmesan Papadelle",
+        price: "£14.50",
+        image: "./assets/images/papadelle.jpg",
         description: "Aenean mattis nisl erat, at porta ipsum dictum non. Morbi pellentesque malesuada ornare. Nunc rhoncus lectus et sollicitudin tempus."
     }
     ,
     {
         id: 6,
-        category: "Lunch",
-        name: "French Toast",
-        price: "£12.50",
-        image: "./assets/images/frenchToast.jpg",
+        category: "Dinner",
+        name: "Seared Lamb Cannon",
+        price: "£18.50",
+        image: "./assets/images/lamb.jpg",
         description: "Aenean mattis nisl erat, at porta ipsum dictum non. Morbi pellentesque malesuada ornare. Nunc rhoncus lectus et sollicitudin tempus."
     },
     {
         id: 7,
         category: "Breakfast",
-        name: "French Toast",
-        price: "£12.50",
-        image: "./assets/images/frenchToast.jpg",
+        name: "Chia Seeds Yoghurt",
+        price: "£7.50",
+        image: "./assets/images/chiaSeedsYoghurt.jpg",
         description: "Aenean mattis nisl erat, at porta ipsum dictum non. Morbi pellentesque malesuada ornare. Nunc rhoncus lectus et sollicitudin tempus."
     }
 
