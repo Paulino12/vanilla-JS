de76302dd3299725356bae69cf6beb029e5535e0
Lino <Pchuitcheu@yahoo.fr>
2022-01-23 23:48:34 +0000

    more work

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ const projectTemplate = (link, image, name) => {
                     <div class="projectTitle">
                         ${name}
                     </div>
+                    <div class="skeleton">
+                        <div class="skeletonChild">
+
+                        </div>
+                    </div>
                 </a>
             </div>
     `
 }
+
+
 
 let projects = vjProjects.map(({ link, image, name }) => {
     return projectTemplate(link, image, name)
 })
 
 jsProjects.innerHTML = projects.join('')
+
+let skeleton = document.getElementsByClassName('skeleton')
+
+for (let i = 0; i < skeleton.length; i++) {
+    setTimeout(() => {
+        skeleton[i].classList.add('removeSekelton')
+    }, 2000);
+
+    setTimeout(() => {
+        skeleton[i].classList.add('adjustSkeleton')
+    }, 3000);
+}
+
